fb9e94711c372343dccf41554ddd68834864a366
Dell <Dell@DESKTOP-3MVNE44>
2021-09-22 21:23:23 +0500

    Initial Commit

--- bank/src/test/java/bank/account_test.java
+++ bank/src/test/java/bank/account_test.java
@@ -11,12 +11,15 @@ public class account_test
 	public account obj;
 	public account obj2;
 	public customer client;
+	public customer client2;
 	
 	@Before
 	public void setUp()
 	{
 		client = new customer (1,"Abdurrafay","fast",0610);
+		client2 = new customer (2,"Gulsher","fast",0445);
 		obj = new account (client,100);
+		obj2 = new account (client2,200);
 	}
 
 	@Test
@@ -27,6 +30,9 @@ public class account_test
 		
 		bal = obj.makeDeposit(-1000);
 		Assert.assertEquals(600, bal);
+		
+		obj.transferAmount(obj2, 600);
+		Assert.assertEquals(800,obj2.checkBalance());
 		
 //		fail("Not yet implemented");
 	}
--- bank/src/test/java/bank/saving_test.java
+++ bank/src/test/java/bank/saving_test.java
@@ -51,5 +51,4 @@ public class saving_test
 		
 //		fail("Not yet implemented");
 	}
-
 }
