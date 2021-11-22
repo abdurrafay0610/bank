6ebb779cc0a2a6f2ef03aa42ccc03f831991fc4f
Dell <Dell@172.16.49.1>
2021-11-22 09:43:12 +0500

    Initial Commit

--- bank/src/test/java/bank/AllTest.java
+++ bank/src/test/java/bank/AllTest.java
@@ -6,7 +6,7 @@ import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
 @SuiteClasses({ account_test.class, checking_test.class, saving_test.class })
-public class AllTests 
+public class AllTest
 {
 
 }
